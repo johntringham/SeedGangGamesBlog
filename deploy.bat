34b66681c9c4589b0ac55fd26d63a43c1da4d9fa
John Tringham <john@tringham.co.uk>
2022-02-22 15:41:54 +0000

    Deployment autocommit - 22/02/2022 15:41:54.18

---
+++ deploy.bat
@@ -0,0 +1,10 @@
+echo off
+echo doing git stuff now:
+
+git add -A
+git commit -m "Deployment autocommit - %date% %time%
+
+echo "Going to deploy now"
+hexo clean & hexo generate & echo blog.seedganggames.com > public/CNAME & surge --project public
+
+set /p DUMMY=Hit ENTER to continue...
