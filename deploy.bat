5a842e236563beb31f4b4c2c93b0ecd4af4ce7fb
john (laptop) <john@tringham.co.uk>
2022-07-13 11:30:51 +0100

    testing push stuff one sec

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,7 @@ echo doing git stuff now:
 
 git add -A
 git commit -m "Deployment autocommit - %date% %time%
+git push
 
 echo "Going to deploy now"
 hexo clean & hexo generate & echo blog.seedganggames.com > public/CNAME & surge --project public
