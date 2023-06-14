dd2a43d41b40cc32cad05db676564b1fa5064e28
John Tringham <john@tringham.co.uk>
2023-06-14 15:04:56 +0100

    Deployment autocommit - 14/06/2023 15:04:48.06

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 echo off
 echo doing git stuff now:
 echo "Going to deploy now"
-hexo clean & hexo generate & git add -A & git commit -m "Deployment autocommit - %date% %time% & git push
+hexo clean & hexo generate & git add -A & git commit -m "Deployment autocommit - %date% %time%" & git push
 set /p DUMMY=Hit ENTER to continue...
