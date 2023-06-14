cfdeac75e80db3ea97ecdb586d2e4ecb00641cf4
John Tringham <john@tringham.co.uk>
2023-06-14 15:10:10 +0100

    Deployment autocommit - 14/06/2023 15:10:02.24

--- build.bat
+++ build.bat
@@ -2,11 +2,3 @@ echo off
 
 echo Building...
 hexo clean & hexo generate
-
-echo Committing...
-
-git add -A
-git commit -m "Deployment autocommit - %date% %time%
-git push
-
-set /p DUMMY=Hit ENTER to continue...
